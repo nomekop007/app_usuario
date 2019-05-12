1fef5bc734e526f437dc1426ca63b823ee1f9c50
diego <d.riosrojas@gmail.com>
2019-05-12 19:08:25 -0400

    registrar y logeo normal listo

--- app/src/main/java/com/example/app_usuario_v2/LoginActivity.java
+++ app/src/main/java/com/example/app_usuario_v2/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.app_usuario_v2;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,18 +17,35 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
-        //librerias del cliente de google
-        private GoogleApiClient googleApiClient;
-        private SignInButton signInButton;
 
 
-        public static final int SIGN_IN_CODE = 777;
+    public static int TIPO_LOGEO = 0;
+    // 1 es logeo normal
+    // 2 es logeo con google
+    // 3 es logeo con facebook
 
-          EditText t_correo, t_pass;
+    //librerias del cliente de google
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
 
+    //declarar objeto firebaseAuth
+    private FirebaseAuth firebaseAuth;
+
+    //declarar circulo de cargando
+    private ProgressDialog progressDialog;
+
+
+    public static final int SIGN_IN_CODE = 777;
+
+    EditText t_correo, t_pass;
 
 
     @Override
@@ -35,20 +53,17 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        t_correo = findViewById(R.id.txt_correo);
-        t_pass = findViewById(R.id.txt_pass);
-
+        //iniciar progressDialig
+        progressDialog = new ProgressDialog(this);
 
         logeoConGmail();
 
     }
 
-
-
-
+            //logearse con cuenta creada
     public void iniciar(View view) {
-
-
+        t_correo = findViewById(R.id.txt_correo);
+        t_pass = findViewById(R.id.txt_pass);
 
         String correo = t_correo.getText().toString();
         String pass = t_pass.getText().toString();
@@ -62,9 +77,41 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             t_pass.requestFocus();
         } else {
 
+            //cuadro de espera
+            progressDialog.setMessage("Validando...");
+            progressDialog.show();
+
+
+            //inicializar el objeto firebaseAuth
+            firebaseAuth = FirebaseAuth.getInstance();
+
+            firebaseAuth.signInWithEmailAndPassword(correo,pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                Toast.makeText(LoginActivity.this, "Bienvenido "+user.getEmail(), Toast.LENGTH_SHORT).show();
+                                //metodo para redireccionar al main
+
+                                TIPO_LOGEO=1;
+
+                                 goMainScreen(TIPO_LOGEO);
+                            }else {
+                                Toast.makeText(LoginActivity.this, "nombre o contraseña incorrecto", Toast.LENGTH_SHORT).show();
+                             t_pass.setText("");
+                            }
+
+                            progressDialog.dismiss();
+                        }
+
+                    });
 
         }
     }
+
+
 
     public void registrar(View view) {
         Intent intent = new Intent(this, RegistrarActivity.class);
@@ -72,55 +119,59 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
 
+    // inicio de logeo con cuenta gmail
+    public void logeoConGmail() {
 
 
-
-        // inicio de logeo con cuenta gmail
-    public void logeoConGmail(){
         signInButton = findViewById(R.id.btn_gmail);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
 
-        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
 
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //cuadro de espera
+                progressDialog.setMessage("iniciando Sesion con Google...");
+                progressDialog.show();
+
                 Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-                startActivityForResult(intent,SIGN_IN_CODE);
+                startActivityForResult(intent, SIGN_IN_CODE);
             }
         });
     }
-
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 777){
-           GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-           handleSignInResult(result);
+        if (requestCode == 777) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
 
         }
 
     }
-
     private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()){
-            goMainScreen();
-        }else {
+        if (result.isSuccess()) {
 
-            Toast.makeText(this,"no se puede iniciar sesion",Toast.LENGTH_LONG).show();
+            TIPO_LOGEO = 2;
+            goMainScreen(TIPO_LOGEO);
+        } else {
+
+            Toast.makeText(this, "no se puede iniciar sesion", Toast.LENGTH_LONG).show();
+
         }
+        progressDialog.dismiss();
     }
-
-    private void goMainScreen() {
+    private void goMainScreen(int tipo) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("TIPO_LOGEO",tipo+"");
         startActivity(intent);
 
     }
--- app/src/main/java/com/example/app_usuario_v2/MainActivity.java
+++ app/src/main/java/com/example/app_usuario_v2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.app_usuario_v2;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -25,14 +26,26 @@ import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    public static int TIPO_LOGEO = 0;
+    // 1 es logeo normal
+    // 2 es logeo con google
+    // 3 es logeo con facebook
 
 
-        //clase de login gmail
+    //clase de login gmail
     private GoogleApiClient googleApiClient;
 
 
+    //declarar objeto firebaseAuth
+    private FirebaseAuth firebaseAuth;
 
     //declarar elementos de drawer
     private DrawerLayout drawerLayout;
@@ -41,36 +54,62 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private NavigationView navigationView;
     private View header;
 
-
+    //declarar circulo de cargando
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configuraciondeLoginGmail();
+        //extraer tipo de logeo
+        Intent i = getIntent();
+        TIPO_LOGEO = Integer.parseInt(i.getStringExtra("TIPO_LOGEO"));
+
+        //iniciar progressDialig
+        progressDialog = new ProgressDialog(this);
+
+
         configuracionDrawer();
+
+        if (TIPO_LOGEO == 2) {
+            configuraciondeLoginGmail();
+        } else if (TIPO_LOGEO == 3) {
+            //configuracionLoginFacebook();
+        } else {
+            configurardeLoginNormal();
+
+        }
 
     }
 
 
-
-        // se inicializa y configura las opciones del drawer
-    public void configuracionDrawer(){
-        drawerLayout =  findViewById(R.id.drawer_layout);
-        toolbar =  findViewById(R.id.toolbar);
+    // se inicializa y configura las opciones del drawer
+    public void configuracionDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
         navigationView = findViewById(R.id.navigation);
-        header = ((NavigationView)findViewById(R.id.navigation)).getHeaderView(0);
+        header = ((NavigationView) findViewById(R.id.navigation)).getHeaderView(0);
 
         //configuracion del comportamiento del drawer
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.cerrar:
-                        Toast.makeText(MainActivity.this, "sesion cerrada!", Toast.LENGTH_SHORT).show();
-                      cerrarSesion();
+                        //cuadro de espera
+                        progressDialog.setMessage("cerrando sesion");
+                        progressDialog.show();
+
+                        if (TIPO_LOGEO == 2) {
+                            cerrarSesionGoogle();
+                        } else if (TIPO_LOGEO == 3) {
+                            //cerrarSesionFacebook();
+                        } else {
+                            cerrarSesion();
+                        }
+
                         break;
                 }
                 return true;
@@ -79,23 +118,34 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         toolbar.setTitle("Mapa");
         setSupportActionBar(toolbar);
-        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer,R.string.closeDrawer);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
         drawerLayout.setDrawerListener(toggle);
         toggle.syncState();
     }
 
 
+    public void configurardeLoginNormal() {
+        //inicializar el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        Log.e("display name : ", user.getDisplayName() + "");
+        Log.e("email : ", user.getEmail() + "");
+        Log.e("providerid : ", user.getProviderId() + "");
+        Log.e("UID : ", user.getUid() + "");
+        Log.e("numero : ", user.getPhoneNumber() + "");
+
+    }
 
 
     // metodos para logearse con cuenta gmail
-
-    public void configuraciondeLoginGmail(){
+    public void configuraciondeLoginGmail() {
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
 
         googleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+                .enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
 
 
     }
@@ -103,27 +153,27 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     protected void onStart() {
         super.onStart();
+        if (TIPO_LOGEO == 2) {
+            OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+            if (opr.isDone()) {
+                GoogleSignInResult result = opr.get();
+                handSignInResult(result);
 
-        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
-        if(opr.isDone()){
-            GoogleSignInResult result = opr.get();
-            handSignInResult(result);
+            } else {
+                opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                    @Override
+                    public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                        handSignInResult(googleSignInResult);
+                    }
+                });
 
-        }else{
-            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                @Override
-                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
-                    handSignInResult(googleSignInResult);
-                }
-            });
-
+            }
         }
-
     }
 
     //metodo donde se extrae al usuario logeado
     private void handSignInResult(GoogleSignInResult result) {
-        if(result.isSuccess()){
+        if (result.isSuccess()) {
 
             GoogleSignInAccount account = result.getSignInAccount();
 
@@ -132,7 +182,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             ((TextView) header.findViewById(R.id.gmail)).setText(account.getEmail());
             ((TextView) header.findViewById(R.id.code)).setText(account.getId());
 
-        }else {
+        } else {
             goLoginInSreen();
         }
 
@@ -141,32 +191,47 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private void goLoginInSreen() {
         Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        Toast.makeText(MainActivity.this, "sesion cerrada!", Toast.LENGTH_SHORT).show();
+
         startActivity(intent);
 
     }
+
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
     }
-    private void cerrarSesion() {
+
+    private void cerrarSesionGoogle() {
         Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
             @Override
             public void onResult(@NonNull Status status) {
-                if ((status.isSuccess())){
+                if ((status.isSuccess())) {
                     goLoginInSreen();
-                }else {
-                    Toast.makeText(getApplicationContext(),"no se pudo cerrar sesion",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "no se pudo cerrar sesion", Toast.LENGTH_LONG).show();
                 }
+                progressDialog.dismiss();
             }
         });
-    }
 
+    }
     // fin de metodos
 
-
-
+    private void cerrarSesion() {
+        firebaseAuth.getInstance().signOut();
+        goLoginInSreen();
+        progressDialog.dismiss();
+    }
 
     @Override
     public void onBackPressed() {
-        cerrarSesion();
+        if (TIPO_LOGEO == 2) {
+            cerrarSesionGoogle();
+        } else if (TIPO_LOGEO == 3) {
+            //cerrarSesionFacebook();
+        } else {
+            cerrarSesion();
+        }
+
     }
 }
